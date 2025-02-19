5b9418733c7450d094ccc95ff72435de6589b6cd
bryajosepr <bryanjose431@gmail.com>
2025-02-19 14:30:38 -0400

    the 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return:Always 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		if (d <= '8')
+		{
+			putchar(',');
+			putchar(',');
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
