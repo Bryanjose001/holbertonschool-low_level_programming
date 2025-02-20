2fb3a5bd9138a344884a2654437143d1e55c199a
bryajosepr <bryanjose431@gmail.com>
2025-02-20 15:01:36 -0400

    Added file 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,16 +2,11 @@
 /**
  * _islower - lowercase character
  * @c: variable given
- * Return: 0(success)
+ * Return: 1 if character is lowercase, 0 otherwise
  */
-
 int _islower(int c)
 {
-
 	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
+		return (1);	
 	return (0);
 }
