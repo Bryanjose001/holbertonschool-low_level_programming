10c3f34f4a4db0ebfe55374d40cf542156879e8c
bryajosepr <bryanjose431@gmail.com>
2025-02-25 13:51:30 -0400

    the 4

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "manin.h"
+/**
+ * more_numbers -print 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0; j <= 14; j++)
+		{
+			if  (j >= 10)
+			{
+			_putchar((j / 10) + '0')j++)
+			}
+	_putchar((j % 10) + '0');
+		}
+	}
+}
+
