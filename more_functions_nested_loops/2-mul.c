af1078c575d21b7c29e9edc6a8fdf5509f842ea2
bryajosepr <bryanjose431@gmail.com>
2025-02-25 10:43:36 -0400

    the 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiple integers
+ * @a: variable a
+ * @b: varible b
+ * Return: the result
+ */
+int mul(int a int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
