722a52163cbdccfe352023ebdcac0ef9c8d0e0e9
bryajosepr <bryanjose431@gmail.com>
2025-02-25 10:36:58 -0400

    the 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - check for digit
+ * @c: variable check
+ * Return: 1 if c is a digit, 0 otherwie
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
