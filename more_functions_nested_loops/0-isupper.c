5da7e2abb0b8463c51c7bcbb2addcec512d88a04
bryajosepr <bryanjose431@gmail.com>
2025-02-25 10:28:52 -0400

    the 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,12 @@
 
 /**
  * _isupper - check for uppercase char
+ * @c:variable given
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
 	if  (c >= 'A' && c <= 'Z')
 		return (1);
-	return (0);
+return (0);
 }
