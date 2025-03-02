feb3f1c5c998d9e984a2f024071fc5fb54cba326
bryajosepr <bryanjose431@gmail.com>
2025-03-02 13:13:37 -0400

    the 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,7 +4,7 @@
 /**
 *print_array - pritns n elements of an array  of integers
 *@a: array of integers
-*@: number of elements of the array to be printed
+*@n: number of elements of the array to be printed
 *Return: void
 */
 void print_array(int *a, int n)
